199ecebf34e0c341f331cb99af43b960e66a12df
zmcdaniel <zmcdaniel@gmail.com>
2016-07-20 15:58:01 -0700

    Solved problem 1. Counted all results that have a 'kind' of 'shopping#product'. Spoiler alert: it was all 25.'

--- js/google-shopping.js
+++ js/google-shopping.js
@@ -1,5 +1,14 @@
 var data = require('../products.json');
 
-// Write your solutions below
+// 1.) Go through the items and find all results that have kind of shopping#product. 
+// Print the count of these results. Where else is this count information stored in the search results?
+var count = 0;
+for (var i = 0; i < data.currentItemCount; i++) {
+	if (data.items[i].kind === 'shopping#product') {
+		count++;
+	}
+}
+console.log(count);
 
-console.log(data.items);
+
+//console.log(data.items);
