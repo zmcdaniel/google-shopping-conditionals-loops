ab6b3fddfce96042a743f254237d52ab250f847a
zmcdaniel <zmcdaniel@gmail.com>
2016-07-20 16:12:36 -0700

    Completed the 2nd problem. Found and printed the title of all items with a backorder availability

--- js/google-shopping.js
+++ js/google-shopping.js
@@ -1,14 +1,21 @@
 var data = require('../products.json');
 
-// 1.) Go through the items and find all results that have kind of shopping#product. 
-// Print the count of these results. Where else is this count information stored in the search results?
-var count = 0;
+// // 1.) Go through the items and find all results that have kind of shopping#product. 
+// // Print the count of these results. Where else is this count information stored in the search results?
+// var count = 0;
+// for (var i = 0; i < data.currentItemCount; i++) {
+// 	if (data.items[i].kind === 'shopping#product') {
+// 		count++;
+// 	}
+// }
+// console.log(count);
+
+// 2.) Print the title all items with a backorder availability in inventories.
+
 for (var i = 0; i < data.currentItemCount; i++) {
-	if (data.items[i].kind === 'shopping#product') {
-		count++;
+	if (data.items[i].product.inventories[0].availability === 'backorder') {
+		console.log(data.items[i].product.title);
 	}
 }
-console.log(count);
-
 
 //console.log(data.items);
