b74e2a6a1494b0d54157cbee53d92705275de37f
zmcdaniel <zmcdaniel@gmail.com>
2016-07-20 16:18:23 -0700

    Found a solution to question 3, printing the title of any item with more than one image.

--- js/google-shopping.js
+++ js/google-shopping.js
@@ -10,10 +10,18 @@ var data = require('../products.json');
 // }
 // console.log(count);
 
-// 2.) Print the title all items with a backorder availability in inventories.
+// // 2.) Print the title all items with a backorder availability in inventories.
+
+// for (var i = 0; i < data.currentItemCount; i++) {
+// 	if (data.items[i].product.inventories[0].availability === 'backorder') {
+// 		console.log(data.items[i].product.title);
+// 	}
+// }
+
+// 3.) Print the title all items with more than one image link.
 
 for (var i = 0; i < data.currentItemCount; i++) {
-	if (data.items[i].product.inventories[0].availability === 'backorder') {
+	if (data.items[i].product.images.length > 1) {
 		console.log(data.items[i].product.title);
 	}
 }
